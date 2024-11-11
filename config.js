915fea52e0efadea68e5d85c538a265e21a3f1f4
zeviko17 <cityjeruslaem@gmail.com>
2024-11-11 21:08:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const CONFIG = {
     SHEET_ID: "10IkkOpeD_VoDpqMN23QFxGyuW0_p0TZx4NpWNcMN-Ss",
-    SHEET_NAME: "קבוצות להודעות"
+    SHEET_NAME: "קבוצות להודעות",
+    SEND_MESSAGE_URL: "https://7103.api.greenapi.com/waInstance7103962196/sendMessage/64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae",
+    SEND_FILE_URL: "https://7103.api.greenapi.com/waInstance7103962196/sendFileByUrl/64e3bf31b17246f1957f8935b45f7fb5dc5517ee029d41fbae"
 };
